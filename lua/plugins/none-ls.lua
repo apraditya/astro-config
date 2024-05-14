c1d0ec5e5f8bb0f268a2ea714b1a4149228215e0
Adinda Praditya <apraditya@gmail.com>
2024-05-14 22:31:37 +0700

    Activate mason and none-ls plugins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize None-ls sources
 
 ---@type LazySpec
@@ -7,15 +5,22 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
+      -- Set a code actions
+      null_ls.builtins.code_actions.eslint_d,
       -- Set a formatter
+      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.formatting.fixjson,
+      null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.rubocop,
+      null_ls.builtins.formatting.stylelint,
       -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
     }
     return config -- return final config table
   end,
